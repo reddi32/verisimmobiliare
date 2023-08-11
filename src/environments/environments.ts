de9dd73a7c4b86852f7f5a39f8550d6805e0db2b
reddi32 <rossimatteo32@gmail.com>
2023-08-11 16:22:03 +0200

    upload

---
+++ src/environments/environments.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    spaceId: 'i5j1ijnpcg10',
+    accessToken: 'iMrP_rTLTK_8tPQlDrxcfE6lYI5DI9Ao0kCmgus3NJQ',
+};
+
+export const affitti = {
+    production: false,
+    spaceIdTwo: 'z0v9guayg2u3',
+    accessTokenTwo: 'FZ1VnW2TWHl40zeIGDHxAckVqBKbI9C7wQbhmTSz7Ck',
+};
