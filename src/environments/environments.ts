54e987b0cff59ef22006dea654bb6b245c4291b9
reddi32 <rossimatteo32@gmail.com>
2025-10-26 10:40:10 +0100

    modifica email contentful e logica dei servizi

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,11 +1,16 @@
 export const environment = {
     production: false,
-    spaceId: 'i5j1ijnpcg10',
-    accessToken: 'iMrP_rTLTK_8tPQlDrxcfE6lYI5DI9Ao0kCmgus3NJQ',
+    spaceId: '1dvtjeqazn3t',
+    accessToken: 'L1VJ8Cs91J4DS4eg7z240PXhUjkRSiV9Q6d3pXg-c9U',
 };
 
-export const affitti = {
+/*export const affitti = {
     production: false,
     spaceIdTwo: 'z0v9guayg2u3',
     accessTokenTwo: 'FZ1VnW2TWHl40zeIGDHxAckVqBKbI9C7wQbhmTSz7Ck',
+};*/
+export const affitti = {
+    production: false,
+    spaceIdTwo: '1dvtjeqazn3t',
+    accessTokenTwo: 'M22VG-ZqhsHw1n6MO9qCHhrmGLA2yllDqBQd-SvyS4U',
 };
